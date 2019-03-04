295e9d2dd6d8a2b0d0ec90001ca1ad89f777ea91
Buse ERKUŞ <buceerkus@icloud.com>
2019-03-04 21:51:25 +0300

    Initial Commit

---
+++ SearchingTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchingTable
+//
+//  Created by Buse ERKUŞ on 4.03.2019.
+//  Copyright © 2019 Buse ERKUŞ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
